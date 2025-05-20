10e8384ab4100658f2a8a30d0e664f26435a7278
Reza Kherad <kherad148@gmail.com>
2025-05-20 13:54:12 +0330

    add Home

--- Restaurant-Landing-Page-Tutorial-main/src/Components/Home.js
+++ Restaurant-Landing-Page-Tutorial-main/src/Components/Home.js
@@ -6,33 +6,30 @@ import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
   return (
-
-
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Hot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeding, chopping
+            & marinating, so you can cook a fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Home;
-
-    // <div className="home-container">
-    //   <Navbar />
-    //   <div className="home-banner-container">
-    //     <div className="home-bannerImage-container">
-    //       <img src={BannerBackground} alt="" />
-    //     </div>
-    //     <div className="home-text-section">
-    //       <h1 className="primary-heading">
-    //         Your Favourite Food Delivered Hot & Fresh
-    //       </h1>
-    //       <p className="primary-text">
-    //         Healthy switcher chefs do all the prep work, like peeding, chopping
-    //         & marinating, so you can cook a fresh food.
-    //       </p>
-    //       <button className="secondary-button">
-    //         Order Now <FiArrowRight />{" "}
-    //       </button>
-    //     </div>
-    //     <div className="home-image-section">
-    //       <img src={BannerImage} alt="" />
-    //     </div>
-    //   </div>
-    // </div>
