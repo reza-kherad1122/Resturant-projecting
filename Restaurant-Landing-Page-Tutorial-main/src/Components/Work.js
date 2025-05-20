d55b0e089326f249edfd72b051a1cb956347b88a
Reza Kherad <kherad148@gmail.com>
2025-05-20 15:15:50 +0330

    Add Work Page

--- Restaurant-Landing-Page-Tutorial-main/src/Components/Work.js
+++ Restaurant-Landing-Page-Tutorial-main/src/Components/Work.js
@@ -4,21 +4,21 @@ import ChooseMeals from "../Assets/choose-image.png";
 import DeliveryMeals from "../Assets/delivery-image.png";
 
 const Work = () => {
-  const workInfoData = [
+  const Maching = [
     {
-      image: PickMeals,
+      img: PickMeals,
       title: "Pick Meals",
       text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
     },
     {
-      image: ChooseMeals,
-      title: "Choose How Often",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+      img: ChooseMeals,
+      title: "Choose Meals",
+      text: "Lorem ipsum dolor sit amet consectetur.",
     },
     {
-      image: DeliveryMeals,
-      title: "Fast Deliveries",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+      img: DeliveryMeals,
+      title: "Delivery Meals",
+      text: " Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
     },
   ];
   return (
@@ -27,19 +27,19 @@ const Work = () => {
         <p className="primary-subheading">Work</p>
         <h1 className="primary-heading">How It Works</h1>
         <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+          Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis
+          elementum interdum facilisi bibendum
         </p>
       </div>
       <div className="work-section-bottom">
-        {workInfoData.map((data) => (
+        {Maching.map((data) => (
           <div className="work-section-info" key={data.title}>
             <div className="info-boxes-img-container">
-              <img src={data.image} alt="" />
+              <img src={data.img} alt="/'/'/'"/>
+              </div>
+              <h1>{data.title}</h1>
+              <p>{data.text}</p>
             </div>
-            <h2>{data.title}</h2>
-            <p>{data.text}</p>
-          </div>
         ))}
       </div>
     </div>
