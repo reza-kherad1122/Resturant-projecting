b5c8de0c9556fdc88da8822b0dd58b1ceaec12c9
Reza Kherad <kherad148@gmail.com>
2025-05-21 16:31:37 +0330

    Add a Page Contact

--- Restaurant-Landing-Page-Tutorial-main/src/Components/Contact.js
+++ Restaurant-Landing-Page-Tutorial-main/src/Components/Contact.js
@@ -14,3 +14,12 @@ const Contact = () => {
 };
 
 export default Contact;
+
+    // <div className="contact-page-wrapper">
+    //   <h1 className="primary-heading">Have Question In Mind?</h1>
+    //   <h1 className="primary-heading">Let Us Help You</h1>
+    //   <div className="contact-form-container">
+    //     <input type="text" placeholder="yourmail@gmail.com" />
+    //     <button className="secondary-button">Submit</button>
+    //   </div>
+    // </div>
